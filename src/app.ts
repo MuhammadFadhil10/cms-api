e5ec4418ad1dd0fb81de1937eeed7d9626794497
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-05-11 17:14:42 +0700

    chore: initial commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import "module-alias/register";
+
+import type { Request, Response } from "express";
+
+import express from "express";
+import { config } from "dotenv";
+
+// config
+config();
+
+// utils
+const PORT = process.env.PORT;
+const API_PREFIX = "/api/v1";
+
+// server
+const app = express();
+
+app.get(`${API_PREFIX}/test-server-run`, (req: Request, res: Response) => {
+  res.send("Server run succesfully ");
+});
+
+app.listen(PORT, () => {
+  console.log("app listening on port: ", PORT, " 🚀🚀🚀");
+});
