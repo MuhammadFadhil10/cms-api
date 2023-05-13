79841a35fcf957bdae8fb5a64632d4c10e69c959
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-05-13 17:38:43 +0700

    feat: delete auth middleware test routes

--- src/api/routes/users.routes.ts
+++ src/api/routes/users.routes.ts
@@ -3,10 +3,8 @@ import express from "express";
 const router = express.Router();
 
 import { signup, signin } from "@/api/controllers";
-import authMiddleware from "@/api/middleware/auth.middleware.ts";
 
 router.post("/user/signup", signup);
 router.post("/user/signin", signin);
-router.get("/user/test-auth-middleware", authMiddleware);
 
 export default router;
