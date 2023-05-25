c938f583bfa2aeb418da62ba061456361d7fe1ee
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-05-25 14:26:54 +0700

    fix: sign in response status

--- src/api/controllers/users.controller.ts
+++ src/api/controllers/users.controller.ts
@@ -67,7 +67,7 @@ export const signin = async (req: Request, res: Response) => {
       },
     );
 
-    return res.status(401).json({
+    return res.status(200).json({
       data: {
         token,
         refreshToken,
